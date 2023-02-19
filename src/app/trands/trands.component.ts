c026765357e0173f2bba20dfba23014400f71730
nada.h <nada.h22425@gmail.com>
2023-02-20 01:49:40 +0200

    add loading in trends

--- src/app/trands/trands.component.ts
+++ src/app/trands/trands.component.ts
@@ -13,6 +13,10 @@ export class TrandsComponent implements OnInit {
   trandcontent: any;
   Trands: any;
   relateTrends:any[]=[]
+  last_news!: any[];
+  more_articles_Read!: any[];
+  loading = true;
+  relatedLoading = true;
   constructor(
     private httpService: HttpService,
     public activatedRoute: ActivatedRoute
@@ -26,12 +30,13 @@ export class TrandsComponent implements OnInit {
   ngOnInit(): void {
     this.getTrandscontent();
     this.getTrands();
+    this.lastNews();
   }
   getTrandscontent() {
     this.httpService.getTrendsContent(this.slug).subscribe(
       (data: any) => {
         console.log(data);
-        
+        this.loading = false;
         this.trandcontent = data.data;
       },
       (err: any) => {}
@@ -51,6 +56,19 @@ export class TrandsComponent implements OnInit {
         console.log(res);
         this.relateTrends=res.data
         
+      }, error: (err: any) => {
+        console.log(err);
+        
+      }
+    })
+  }
+
+  lastNews() {
+    this.httpService.lastNews().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.last_news = res.data.last_news;
+        this.more_articles_Read=res.data.more_articles_Read
       }, error: (err: any) => {
         console.log(err);
         
--- src/app/trands/trends-news/trends-news.component.ts
+++ src/app/trands/trends-news/trends-news.component.ts
@@ -10,7 +10,9 @@ export class TrendsNewsComponent implements OnInit {
 
   trends!: any[];
   last_news!: any[];
-  more_articles_Read!:any[]
+  more_articles_Read!: any[];
+  loading = true;
+  newsLoading = true;
   constructor(
     private httpService: HttpService,) { }
 
@@ -21,10 +23,10 @@ export class TrendsNewsComponent implements OnInit {
   getTrends() {
     this.httpService.getTrends().subscribe({
       next: (res: any) => {
-        console.log(res);
-        
-        this.trends=res.data.trends
-        console.log(this.trends);
+        if (res.success) {
+          this.loading=false
+          this.trends=res.data.trends
+        }
         
       }, error: (err: any) => {
         console.log(err);
@@ -35,9 +37,11 @@ export class TrendsNewsComponent implements OnInit {
   lastNews() {
     this.httpService.lastNews().subscribe({
       next: (res: any) => {
-        console.log(res);
-        this.last_news = res.data.last_news;
-        this.more_articles_Read=res.data.more_articles_Read
+        if (res.success) {
+          this.newsLoading=false
+          this.last_news = res.data.last_news;
+          this.more_articles_Read=res.data.more_articles_Read
+        }
       }, error: (err: any) => {
         console.log(err);
         
