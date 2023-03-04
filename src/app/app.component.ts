907e06ed3ad35885136d58269c3d1d81e0b1a95a
mahmoudelagouz21@gmail.com <abdelwarethms@gmail.com>
2023-03-04 23:28:10 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  
+  <app-navbar></app-navbar>
   <router-outlet></router-outlet>
+  <app-footer></app-footer>
   <app-loader></app-loader>
   
   `,
