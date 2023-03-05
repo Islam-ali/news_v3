8f9b4b77b49723bfa1f17ec7724b572829b5db26
nada.h <nada.h22425@gmail.com>
2023-03-05 22:26:25 +0200

    article details

---
+++ src/app/@category/components/article-details/article-details.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpService } from 'src/app/@core/services/http/http.service';
+
+@Component({
+  selector: 'app-article-details',
+  templateUrl: './article-details.component.html',
+  styleUrls: ['./article-details.component.scss']
+})
+export class ArticleDetailsComponent implements OnInit {
+  slug!: string;
+  selected: any;
+  catId: any;
+  news: any;
+  Trends: any;
+  MoreRead: any;
+
+  constructor(
+    private httpService: HttpService,
+    public activatedRoute: ActivatedRoute
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      this.slug = String(params.get('slug'));
+      this.articleDetail(this.slug);
+      this.getRelated();
+      this.getTrands();
+    });
+  }
+  articleDetail(slug:any) {
+    this.httpService.articleDetails(slug).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.selected = res.data;
+
+        this.catId=this.selected.id;  
+
+        
+      }, error: (err: any) => {
+        console.log(err);
+        
+    }
+  })
+  }
+  getRelated() {
+    this.httpService.getRelatedNews(this.catId).subscribe(
+      (data: any) => {
+        this.news = data.data;
+      },
+      (err: any) => {}
+    );
+  }
+  
+  getTrands() {
+    this.httpService.getTrandsSide().subscribe(
+      (data: any) => {
+        this.Trends = data.data.last_trends;
+        this.MoreRead=data.data.more_articles_Read;
+      },
+      (err: any) => {}
+    );
+  }
+}
