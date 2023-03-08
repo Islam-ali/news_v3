f61558edacf1ada4deab5eee693ee5daed9e9c71
mahmoudelagouz21@gmail.com <abdelwarethms@gmail.com>
2023-03-08 03:18:55 +0200

    home-updates

--- src/app/@shared/components/layout-components/navbar/navbar.component.ts
+++ src/app/@shared/components/layout-components/navbar/navbar.component.ts
@@ -1,21 +1,47 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/@core/services/http/http.service';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { map, share } from 'rxjs/operators';
+import { registerLocaleData } from '@angular/common';
+import localeAr from '@angular/common/locales/ar-EG';
+registerLocaleData(localeAr);
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit,OnDestroy {
 isShown:boolean=false;
 allCategories:any;
 showMore:boolean=false;
-taskBarItems:any[]=[]
+taskBarItems:any[]=[];
+time = new Date();
+  rxTime = new Date();
+  intervalId:any;
+  subscription!: Subscription;
 constructor(private httpService:HttpService){ }
 
   ngOnInit(): void {
     this.getAllCategories();
-    this.getTaskbar()
+    this.getTaskbar();
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+
+    // Using RxJS Timer
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        let hour = this.rxTime.getHours();
+        let minuts = this.rxTime.getMinutes();
+        let seconds = this.rxTime.getSeconds();
+        //let a = time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+        let NewTime = hour+ ":" + minuts + ":" + seconds
+        this.rxTime = time;
+      });
   }
 shDrop()
 {
@@ -44,5 +70,11 @@ getTaskbar() {
 setName(name:string)
 {
   localStorage.setItem('categoryNmame',name)
+}
+ngOnDestroy() {
+  clearInterval(this.intervalId);
+  if (this.subscription) {
+    this.subscription.unsubscribe();
+  }
 }
 }
