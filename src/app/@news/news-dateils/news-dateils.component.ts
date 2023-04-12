9d36387e58d266e6c0d5871af302cc90905a3154
nada.h <nada.h22425@gmail.com>
2023-04-12 03:47:53 +0200

    edits 2

--- src/app/@news/news-dateils/news-dateils.component.ts
+++ src/app/@news/news-dateils/news-dateils.component.ts
@@ -30,7 +30,7 @@ export class NewsDateilsComponent implements OnInit {
   
 
     this.url=this.router.url;
-   this.carrenturl=`https://lhzanews.com/${this.url}`
+   this.carrenturl=`https://lhzanews.com${this.url}`
     console.log(this.carrenturl)
   }
 
